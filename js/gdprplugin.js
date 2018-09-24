1dd4cd93dc9689aa0a417038f4a3bb9ca7e10336
mostrozny <ostrozny.michal@gmail.com>
2018-09-24 13:53:12 +0200

    cookies added, finished work

--- js/gdprplugin.js
+++ js/gdprplugin.js
@@ -7,17 +7,56 @@ window.onload=function() {
     const declineButton = document.querySelector('#gdpr-plugin-button-decline');
 
 
-    //blocking overflow when plugin loaded.
-    body.style.overflow = "hidden";
 
+    //searching cookie by name
+    function showCookie(name) {
+        if (document.cookie !== "") {
+            const cookies = document.cookie.split(/; */);
+
+            for (let i=0; i<cookies.length; i++) {
+                const cookieName = cookies[i].split("=")[0];
+                const cookieVal = cookies[i].split("=")[1];
+                if (cookieName === decodeURIComponent(name)) {
+                    return decodeURIComponent(cookieVal);
+                }
+            }
+        }
+    }
+
+    //checking the cookie
+    function acceptedCookie () {
+       if (showCookie("accepted") === "true") {
+           hideBox();
+       } else {
+           //blocking overflow when plugin loaded.
+           body.style.overflow = "hidden";
+       };
+   }
+    acceptedCookie();
 
     function gdprDeclineAction() {
         hideBox();
         console.log("Decline");
+        Date.prototype.addDays = function(days) {
+            var date = new Date(this.valueOf());
+            date.setDate(date.getDate() + days);
+            return date;
+        }
+        var date = new Date();
+        document.cookie = `accepted=false; expires=${date.addDays(0)}; path=/`;
     }
+
     function gdprAcceptAction() {
         hideBox();
         console.log("Accept");
+        Date.prototype.addDays = function(days) {
+            var date = new Date(this.valueOf());
+            date.setDate(date.getDate() + days);
+            return date;
+        }
+        var date = new Date();
+       document.cookie = `accepted=true; expires=${date.addDays(7)}; path=/`;
+       console.log(document.cookie);
     }
 
     //hidding box function
